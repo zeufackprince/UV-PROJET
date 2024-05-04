63a6b701d0087cbeef6ae6e89709cfd452d3eafa
Prince Levis <tamezelv@gmail.com>
2024-05-04 04:18:14 +0100

    security is not good

--- BACK-END/app/src/main/java/immobilier/app/Security/SecurityConfiguration.java
+++ BACK-END/app/src/main/java/immobilier/app/Security/SecurityConfiguration.java
@@ -1,30 +1,30 @@
-package immobilier.app.Security;
+// package immobilier.app.Security;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+// import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+// import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebFluxSecurity
-public class SecurityConfiguration {
+// @Configuration
+// @EnableWebFluxSecurity
+// public class SecurityConfiguration {
 
-@Bean
-public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
-    try {
-        return 
-        httpSecurity.csrf(AbstractHttpConfigurer::disable).
-                            authorizeHttpRequests(
-                                authotize -> authotize.requestMatchers("/inscription").permitAll()
-                                .anyRequest().authenticated() 
-                            ).build();
-    } catch (Exception e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-        throw new IllegalAccessError("authentification error");
-    }
-}
+// @Bean
+// public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+//     try {
+//         return 
+//         httpSecurity.csrf(AbstractHttpConfigurer::disable).
+//                             authorizeHttpRequests(
+//                                 authotize -> authotize.requestMatchers("/inscription").permitAll()
+//                                 .anyRequest().authenticated() 
+//                             ).build();
+//     } catch (Exception e) {
+//         // TODO Auto-generated catch block
+//         e.printStackTrace();
+//         throw new IllegalAccessError("authentification error");
+//     }
+// }
 
-}
+// }
